e8ddfb14b420e083865a8448daa646b98d042775
DuarteMCSilva <duartemcsilva@gmail.com>
2024-01-06 19:45:02 +0000

    Refactor: Created component folder & cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MasterNavComponent } from './components/master-nav/master-nav.component';
+import { ContentComponent } from './components/main/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
